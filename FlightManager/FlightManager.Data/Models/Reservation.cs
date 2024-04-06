c088e2b1df7d4b23c5658f450e1f0c6eab70bf35
Pepi63 <pepigavazov1@gmail.com>
2024-04-06 11:10:01 +0300

    structure

---
+++ FlightManager/FlightManager.Data/Models/Reservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManager.Data.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int FlightId { get; set; }
+        public User User { get; set; }
+        public Flight Flight { get; set; }
+    }
+}
