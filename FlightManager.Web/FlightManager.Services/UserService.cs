2bd188ffe4a54d894bc77f4c4e15808f3db8a0d9
Pepi63 <pepigavazov1@gmail.com>
2024-04-19 08:18:21 +0300

    s

--- FlightManager.Web/FlightManager.Services/UserService.cs
+++ FlightManager.Web/FlightManager.Services/UserService.cs
@@ -13,164 +13,164 @@ using System.Threading.Tasks;
 
 namespace FlightManager.Services
 {
-    //public class UserService : IUserService
-    //{
-        //private readonly UserManager<User> userManager;
-        //private readonly ApplicationDbContext context;
-        //private readonly RoleManager<IdentityRole> roleManager;
-        //private readonly SignInManager<User> signInManager;
-        //private const int ItemsCount = 0;
+    public class UserService : IUserService
+    {
+        private readonly UserManager<User> userManager;
+        private readonly ApplicationDbContext context;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly SignInManager<User> signInManager;
+        private const int ItemsCount = 0;
 
-        //public UserService(UserManager<User> userManager, ApplicationDbContext context, RoleManager<IdentityRole> roleManager, SignInManager<User> signInManager)
-        //{
-        //    this.userManager = userManager;
-        //    this.context = context;
-        //    this.roleManager = roleManager;
-        //    this.signInManager = signInManager;
-        //}
+        public UserService(UserManager<User> userManager, ApplicationDbContext context, RoleManager<IdentityRole> roleManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.context = context;
+            this.roleManager = roleManager;
+            this.signInManager = signInManager;
+        }
 
-        //public async Task<string> CreateUserAsync(CreateUserViewModel model)
-        //{
-        //    User user = new User()
-        //    {
-        //        UserName = model.Email,
-        //        Email = model.Email,
-        //        FirstName = model.FirstName,
-        //        LastName = model.LastName,
-        //        NationalId = model.NationalId,
-        //        Address = model.Address,
-        //        UCN = model.UCN,
-        //        PhoneNumber = model.PhioneNumber,
-        //        MainRole=model.Role
-        //    };
+        public async Task<string> CreateUserAsync(CreateUserViewModel model)
+        {
+            User user = new User()
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                NationalId = model.NationalId,
+                Address = model.Address,
+                UCN = model.UCN,
+                PhoneNumber = model.PhioneNumber,
+                MainRole = model.Role
+            };
 
-        //    var result = await userManager.CreateAsync(user, model.Password);
-        //    if (result.Succeeded)
-        //    {
-        //        if (userManager.Users.Count() <= 1)
-        //        {
-        //            IdentityRole roleAdmin = new IdentityRole() { Name = GlobalConstants.AdminRole };
-        //            IdentityRole roleUser = new IdentityRole() { Name = GlobalConstants.UserRole };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                if (userManager.Users.Count() <= 1)
+                {
+                    IdentityRole roleAdmin = new IdentityRole() { Name = GlobalConstants.AdminRole };
+                    IdentityRole roleUser = new IdentityRole() { Name = GlobalConstants.UserRole };
 
-        //            await roleManager.CreateAsync(roleAdmin);
-        //            await roleManager.CreateAsync(roleUser);
-        //            await userManager.AddToRoleAsync(user, GlobalConstants.AdminRole);
-        //            user.MainRole = GlobalConstants.AdminRole;
-        //        }
-        //        else
-        //        {
-        //            await userManager.AddToRoleAsync(user, GlobalConstants.UserRole);
-        //            user.MainRole = GlobalConstants.UserRole;
-        //        }
-        //        await userManager.UpdateAsync(user);
-        //    }
-        //    return user.Id;
-        //}
+                    await roleManager.CreateAsync(roleAdmin);
+                    await roleManager.CreateAsync(roleUser);
+                    await userManager.AddToRoleAsync(user, GlobalConstants.AdminRole);
+                    user.MainRole = GlobalConstants.AdminRole;
+                }
+                else
+                {
+                    await userManager.AddToRoleAsync(user, GlobalConstants.UserRole);
+                    user.MainRole = GlobalConstants.UserRole;
+                }
+                await userManager.UpdateAsync(user);
+            }
+            return user.Id;
+        }
 
-        //public async Task<bool> DeleteUserAsync(string id)
-        //{
-        //    User? user = await GetUserByIdAsync(id);
-        //    if (user != null)
-        //    {
-        //        var result = await userManager.DeleteAsync(user);
-        //        return result.Succeeded;
-        //    }
-        //    return false;
-        //}
+        public async Task<bool> DeleteUserAsync(string id)
+        {
+            User? user = await GetUserByIdAsync(id);
+            if (user != null)
+            {
+                var result = await userManager.DeleteAsync(user);
+                return result.Succeeded;
+            }
+            return false;
+        }
 
-        //public async Task<IndexUsersViewModel> GetUsersAsync(IndexUsersViewModel users)
-        //{
-        //    if (users == null)
-        //    {
-        //        users = new IndexUsersViewModel(0);
-        //    }
-        //    users.ElementsCount = await GetUsersCountAsync();
+        public async Task<IndexUsersViewModel> GetUsersAsync(IndexUsersViewModel users)
+        {
+            if (users == null)
+            {
+                users = new IndexUsersViewModel(0);
+            }
+            users.ElementsCount = await GetUsersCountAsync();
 
-        //    users.Users = await userManager
-        //        .Users
-        //        .Skip((users.Page - 1) * users.ItemsPerPage)
-        //        .Take(users.ItemsPerPage)
-        //        .Select(x => new IndexUserViewModel()
-        //        {
-        //            Id = x.Id,
-        //            FirstName = x.FirstName,
-        //            LastName = x.LastName,
-        //            Role= x.MainRole
-        //        })
-        //        .ToListAsync();
+            users.Users = await userManager
+                .Users
+                .Skip((users.Page - 1) * users.ItemsPerPage)
+                .Take(users.ItemsPerPage)
+                .Select(x => new IndexUserViewModel()
+                {
+                    Id = x.Id,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Role = x.MainRole
+                })
+                .ToListAsync();
 
-        //    return users;
-        //}
+            return users;
+        }
 
-        //public async Task<int> GetUsersCountAsync()
-        //{
-        //    return await userManager.Users.CountAsync();
-        //}
+        public async Task<int> GetUsersCountAsync()
+        {
+            return await userManager.Users.CountAsync();
+        }
 
-        //public async Task<DetailsUserViewModel?> GetUserDetailsAsync(string id)
-        //{
-        //    DetailsUserViewModel? result = null;
+        public async Task<DetailsUserViewModel?> GetUserDetailsAsync(string id)
+        {
+            DetailsUserViewModel? result = null;
 
-        //    User user = await GetUserByIdAsync(id);
+            User user = await GetUserByIdAsync(id);
 
-        //    if (user != null)
-        //    {
-        //        string roles = string.Join(", ", await userManager.GetRolesAsync(user));
+            if (user != null)
+            {
+                string roles = string.Join(", ", await userManager.GetRolesAsync(user));
 
-        //        result = new DetailsUserViewModel()
-        //        {
-        //            Id = user.Id,
-        //            Name = $"{user.FirstName} {user.LastName}",
-        //            Email = user.Email != null ? user.Email : "n/a",
-        //            Role = user.MainRole,
-        //            Address=user.Address,
-        //            PhoneNumber=user.PhoneNumber,
-        //            UCN= user.UCN
-        //        };
-        //    }
-        //    return result;
-        //}
-        //private async Task<User> GetUserByIdAsync(string id)
-        //{
-        //    return await userManager.Users.FirstOrDefaultAsync(u => u.Id == id);
-        //}
+                result = new DetailsUserViewModel()
+                {
+                    Id = user.Id,
+                    Name = $"{user.FirstName} {user.LastName}",
+                    Email = user.Email != null ? user.Email : "n/a",
+                    Role = user.MainRole,
+                    Address = user.Address,
+                    PhoneNumber = user.PhoneNumber,
+                    UCN = user.UCN
+                };
+            }
+            return result;
+        }
+        private async Task<User> GetUserByIdAsync(string id)
+        {
+            return await userManager.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
 
-        //public async Task<EditUserViewModel?> GetUserToEditAsync(string id)
-        //{
-        //    EditUserViewModel? result = null;
+        public async Task<EditUserViewModel?> GetUserToEditAsync(string id)
+        {
+            EditUserViewModel? result = null;
 
-        //    User? user = await GetUserByIdAsync(id);
+            User? user = await GetUserByIdAsync(id);
 
-        //    if (user != null)
-        //    {
-        //        result = new EditUserViewModel()
-        //        {
-        //            Id = user.Id,
-        //            FirstName = user.FirstName,
-        //            LastName = user.LastName,
-        //            PhoneNumber=user.PhoneNumber,
-        //            Address=user.Address
-        //        };
-        //    }
+            if (user != null)
+            {
+                result = new EditUserViewModel()
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    PhoneNumber = user.PhoneNumber,
+                    Address = user.Address
+                };
+            }
 
-        //    return result;
-        //}
+            return result;
+        }
 
-        //public async Task<string> UpdateUserAsync(EditUserViewModel user)
-        //{
-        //    User? oldUser = await GetUserByIdAsync(user.Id);
+        public async Task<string> UpdateUserAsync(EditUserViewModel user)
+        {
+            User? oldUser = await GetUserByIdAsync(user.Id);
 
-        //    if (oldUser != null)
-        //    {
-        //        oldUser.FirstName = user.FirstName;
-        //        oldUser.LastName = user.LastName;
-        //        oldUser.PhoneNumber = user.PhoneNumber;
-        //        oldUser.Address = user.Address;
-        //        await userManager.UpdateAsync(oldUser);
-        //    }
+            if (oldUser != null)
+            {
+                oldUser.FirstName = user.FirstName;
+                oldUser.LastName = user.LastName;
+                oldUser.PhoneNumber = user.PhoneNumber;
+                oldUser.Address = user.Address;
+                await userManager.UpdateAsync(oldUser);
+            }
 
-        //    return user.Id;
-        //}
+            return user.Id;
+        }
     }
 
 }
